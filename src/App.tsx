602e1f56c729ce11bd0581e65c24122fbb730cea
KaiSkjold <skjoldann@gmail.com>
2025-10-01 12:10:12 +0200

    added drone status component

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import WifiModal from "./components/wifiModal";
 import WifiIcon from "./components/wifiIcon";
+import DroneStatus from "./components/droneStatus/droneStatus";
 
 function App() {
   const [isOpen, setIsOpen] = useState(false);
@@ -29,6 +30,7 @@ function App() {
           <div className="flex-1 min-w-32 p-8 bg-gray-50 rounded-lg shadow-md text-center">
             <h3 className="text-xl font-semibold text-gray-800 mb-4">Section 1</h3>
             <p className="text-gray-600 leading-relaxed">Content for first section</p>
+            <DroneStatus status="Enemy" />
           </div>
           <div className="flex-1 min-w-128 p-8 bg-gray-50 rounded-lg shadow-md text-center">
             <h3 className="text-xl font-semibold text-gray-800 mb-4">Section 2</h3>
@@ -49,6 +51,7 @@ function App() {
         </div>
       </div>
     </div>
+
   );
 }
 
