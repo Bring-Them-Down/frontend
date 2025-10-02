a77978a366c7550ed20acb9ccb1e5e074524d173
MikkelFiltenborg <97887345+MikkelFiltenborg@users.noreply.github.com>
2025-10-02 10:06:29 +0200

    Directional Warning Lights Added.

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import WifiModal from "./components/wifiModal";
 import WifiIcon from "./components/wifiIcon";
 import DroneStatus from "./components/droneStatus/droneStatus";
@@ -6,22 +6,46 @@ import AudioVisualiser from "./components/audioVisualiser";
 
 function App() {
   const [isOpen, setIsOpen] = useState(false);
+  const [activeSide, setActiveSide] = useState<"left" | "right" | null>(null);
+
+  useEffect(() => {
+    // Mockdata alternates every 2 seconds.
+    // Replace setInterval with real mic data.
+    const interval = setInterval(() => {
+      setActiveSide((prev) => (prev === "left" ? "right" : "left"));
+    }, 2000);
+
+    return () => clearInterval(interval); // cleanup
+  }, []);
 
   return (
     <div className="min-h-screen relative">
       {/* Main content area */}
       <div className="min-h-screen flex flex-col items-center justify-center p-4 gap-12">
-        {/* Video component container */}
-        <div className="flex justify-center items-center">
-          <video
-            className="max-w-full h-auto rounded-lg shadow-lg"
-            controls
-            width="1024"
-            height="600"
-          >
-            <source src="#" type="video/mp4" />
-            Your browser does not support the video tag.
-          </video>
+
+        <div>
+          {/* Video + overlay container */}
+          <div className="flex justify-center items-center">
+            <div className="relative">
+              <video
+                className="max-w-full h-auto rounded-lg shadow-lg"
+                controls
+                width="1024"
+                height="600"
+              >
+                <source src="#" type="video/mp4" />
+                Your browser does not support the video tag.
+              </video>
+
+              {activeSide === "right" && (
+                <div className="absolute inset-y-0 right-0 w-[10%] h-[100%] pointer-events-none bg-[radial-gradient(ellipse_at_right_center,rgba(249,115,22,1)_10%,rgba(249,115,22,0)_72%)]"></div>
+              )}
+
+              {activeSide === "left" && (
+                <div className="absolute inset-y-0 left-0 w-[10%] h-[100%] pointer-events-none bg-[radial-gradient(ellipse_at_left_center,rgba(249,115,22,1)_10%,rgba(249,115,22,0)_72%)]"></div>
+              )}
+            </div>
+          </div>
         </div>
 
         {/* Three divs container */}
