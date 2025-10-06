31476956f0a9c194bfae24010f232bf0c385159f
KaiSkjold <skjoldann@gmail.com>
2025-10-06 09:01:48 +0200

    added logs button

--- src/App.tsx
+++ src/App.tsx
@@ -77,6 +77,13 @@ function App() {
             {/* WiFi button positioned absolutely */}
             <WifiModal />
             <GalleriModal />
+            <div>
+              <button
+                className='relative flex items-center justify-center font-["Special_Elite"] text-white text-xl rounded-lg p-4 cursor-pointer min-w-[204px] min-h-[68px] border-2 hover:bg-stone-950/35'
+              >
+                <span className="pt-1.5">Logs</span>
+              </button>
+            </div>
           </div>
         </div>
       </div>
